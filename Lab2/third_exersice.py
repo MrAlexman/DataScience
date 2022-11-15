ffc61d50d08c4641cf09e899f0ce9d4e46c94e4c
MrAlexman <lastik-fatcat@yandex.ru>
2022-11-15 21:40:20 +0300

    Changed start values

--- Lab2/third_exersice.py
+++ Lab2/third_exersice.py
@@ -176,18 +176,16 @@ def mover(docs, dirs, doc_s, doc_n):
 
 
 if __name__ == "__main__":
-    # documents = [
-    #     {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-    #     {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-    #     {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
-    # ]
-    # directories = {
-    #     "1": ["2207 876234", "11-2"],
-    #     "2": ["10006"],
-    #     "3": []
-    # }
-    documents = []
-    directories = {}
+    documents = [
+        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+    ]
+    directories = {
+        "1": ["2207 876234", "11-2"],
+        "2": ["10006"],
+        "3": []
+    }
     F = 0
     C = None
     while C != 'q':
